d11485262803313b2188897616cbee751d34be7a
Anthony-dominianni <anthony.dominianni@gmail.com>
2019-01-11 16:07:41 -0500

    first commit

---
+++ src/main/java/com/example/cs4500/sp19/s3/anthonydominianni/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.cs4500.sp19.s3.anthonydominianni;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/example/cs4500/sp19/s3/anthonydominianni/controllers/hello/HelloObject.java
@@ -0,0 +1,24 @@
+package com.example.cs4500.sp19.s3.anthonydominianni.controllers.hello;
+
+public class HelloObject {
+	
+	private String message;
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public HelloObject(String message) {
+		this.message = message;
+	}
+	
+	public HelloObject() {
+		
+	}
+	
+
+}
